0ae6855198bb50529372a26afe7efe5fa5003849
Wellington Damasceno <wellfurtado@gmail.com>
2025-01-25 22:01:07 -0300

    Configurando banco de preferencia

--- app/src/main/java/br/com/livrokotlin/ganheinamega/MainActivity.kt
+++ app/src/main/java/br/com/livrokotlin/ganheinamega/MainActivity.kt
@@ -1,5 +1,6 @@
 package br.com.livrokotlin.ganheinamega
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.util.Log
 import android.view.View
@@ -11,13 +12,25 @@ import androidx.appcompat.app.AppCompatActivity
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var prefs: SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //buscar objetos de tela (activity_main.xml) para ter referências dentro do codigo
         val editText: EditText = findViewById(R.id.edit_number)
         val txtResult: TextView = findViewById(R.id.txt_result)
         val btnGenerate: Button = findViewById(R.id.btn_generate)
+
+        //Banco de dados de PReferencias
+        prefs = getSharedPreferences("db", MODE_PRIVATE)
+        val result = prefs.getString("result", null)
+        if (result != null) {
+            txtResult.text = "Última aposta: $result"
+        }
+
 
         btnGenerate.setOnClickListener {
             val text = editText.text.toString()
@@ -43,6 +56,11 @@ class MainActivity : AppCompatActivity() {
 
             val resultText = numbers.joinToString(" - ") // Formata a saída como "12 - 23 - 45..."
             txtResult.text = resultText // Exibir resultado no TextView
+
+            //Salvando no banco de dados local (Preferencias)
+            val editor = prefs.edit()
+            editor.putString("result", txtResult.text.toString())
+            editor.apply()
 
             Log.i("TESTE", "Números sorteados: $resultText")
         } else {
